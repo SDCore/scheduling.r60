c3e55d6ee29129f59cedd6df34740d782617ef3b
Michael Voell <sdcored@gmail.com>
2015-09-22 21:38:40 -0500

    Main sidebar completed

--- index.php
+++ index.php
@@ -5,12 +5,22 @@
 <head>
 
 	<link rel="stylesheet" type="text/css" href="./assets/css/main.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
 
 <body>
 
-	Hello!
+	<div class="left-nav">
+		<div class="header">
+			<div class="brand">Route 60 Manager</div>
+		</div>
+		<div class="links">
+			<a href="#"><i class="fa fa-home"></i> Home</a>
+			<a href="#"><i class="fa fa-ticket"></i> Tickets</a>
+			<a href="#"><i class="fa fa-search"></i> Search</a>
+		</div>
+	</div>
 
 </body>
 
