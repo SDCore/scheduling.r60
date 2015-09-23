eac9178bbce69f47a2e72c08a0c20a802c8b5ab8
Michael Voell <sdcored@gmail.com>
2015-09-23 12:34:33 -0500

    More CSS and HTML. And CSS.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <head>
 
 	<title>Route 60 Detailing</title>
+	<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="./assets/css/main.css" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
@@ -12,15 +13,51 @@
 
 <body>
 
+	<div class="nav">
+		<a href="#" class="brand">Detail Manager</a>
+
+		<div class="right">
+			<a href="#"><i class="fa fa-cog"></i></a>
+			<a href="#"><i class="fa fa-power-off"></i></a>
+		</div>
+	</div>
+
 	<div class="left-nav">
 		<div class="header">
 			<div class="brand">Route 60 Manager</div>
 		</div>
 		<div class="links">
-			<a href="#"><i class="fa fa-home"></i> Home</a>
+			<a href="#" class="active"><i class="fa fa-home"></i> Home</a>
 			<a href="#"><i class="fa fa-ticket"></i> Tickets</a>
 			<a href="#"><i class="fa fa-search"></i> Search</a>
 		</div>
+	</div>
+
+	<div class="container">
+		<h2 class="welcome">Welcome, Route 60!</h2>
+
+		<div class="row">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-title">Recent Tickets</div>
+					<div class="card-content">
+						asd
+					</div>
+					<div class="card-footer">Footer</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-title">Notifications</div>
+					<div class="card-content">
+						asd
+					</div>
+					<div class="card-footer">Footer</div>
+				</div>
+			</div>
+		</div>
+
 	</div>
 
 </body>
