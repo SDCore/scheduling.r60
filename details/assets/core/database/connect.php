023e36b315ae0931ee1a6bfa575b9bb8bf21f7b4
Michael Voell <sdcored@gmail.com>
2016-03-18 17:23:20 -0500

    Repo fixes? :(

--- details/assets/core/database/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$database_error = "<b>Error:</b> Could not connect to database.";
-	mysql_connect('', 'root', '') or die($database_error);
-	$dbselect_error = "<b>Error:</b> Could not find selected database.";
-	mysql_select_db('details') or die($dbselect_error);
-
-?>
--- details/settings.php
+++
@@ -1,9 +0,0 @@
-<?php require("./assets/core/init.php"); require("./include/navbar.php"); ?>
-
-	<div class="container">
-
-		<center><h1>COMING SOON</h1></center>
-
-	</div>
-
-<?php require("./include/footer.php"); ?>
