796191f039bf395961ef2c1893acf6cf4ebf0eb2
Michael Voell <sdcored@gmail.com>
2016-03-18 17:37:13 -0500

    FIXING AGAIN

--- README.md
+++
@@ -1,3 +0,0 @@
-**Welcome!**
-
-This project is to help simplify the Detail System at Route 60 Auto Wash & Detail. This helps make the switch from paper tickets, which have proven to be unreliable and easily lost, to a website that is easy to use, with a database that is easily accessible and keeps records for future use.
--- details/include/tickets-button.php
+++
@@ -1,7 +0,0 @@
-<a href="<?php echo $site; ?>/new"><div class="button-raised" style="width: auto;"><i class="fa fa-plus"></i> New Ticket</div></a>
-
-<span style="margin-left: 10px; margin-right: 10px;">&middot;</span>
-
-<a href="<?php echo $site; ?>/tickets?page=1"><div class="button-raised" style="width: auto; background: #20B473; font-size: 22px; padding: 5px 10px; margin-right: 5px;"><i class="fa fa-list"></i></div></a>
-
-<a href="<?php echo $site; ?>/scheduled?page=1"><div class="button-raised" style="Width: auto; background: #FFC017; font-size: 22px; padding: 5px 10px;"><i class='fa fa-clock-o'></i></div></a>
