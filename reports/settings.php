ced70ce4cf55ca714cb2752901a2fb2043ca5baf
Michael Voell <sdcored@gmail.com>
2016-03-18 17:15:23 -0500

    Report Manager Setup

---
+++ reports/settings.php
@@ -0,0 +1,9 @@
+<?php require("./assets/core/init.php"); require("./include/navbar.php"); ?>
+
+	<div class="container">
+
+		<center><h1>COMING SOON</h1></center>
+
+	</div>
+
+<?php require("./include/footer.php"); ?>
